a6fcf045d0804b7e98502a3378a100a8cf4abb36
陈红新 <chenhx2021@163.com>
2021-01-28 17:22:53 +0800

    t5

--- HelloGit/src/main/java/com/atguigu/git/T.java
+++ HelloGit/src/main/java/com/atguigu/git/T.java
@@ -3,10 +3,15 @@ package com.atguigu.git;
 public class T {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
         System.out.println("aaaa");
         System.out.println("aaaa");
         System.out.println("aaaa");
         System.out.println("aaaa");
         System.out.println("aaaa");
+
     }
 }
