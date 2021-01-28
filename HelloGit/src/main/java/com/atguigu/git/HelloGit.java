57232eb9300210e3042888974748695163787ee0
陈红新 <chenhx2021@163.com>
2021-01-28 17:19:48 +0800

    t4

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -2,14 +2,6 @@ package com.atguigu.git;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("更新1");
-        System.out.println("创建分支");
-        System.out.println("分支添加");
-        System.out.println("dev_test3");
-        System.out.println("dev1");
-        System.out.println("dev2");
-        System.out.println("dev3");
-        System.out.println("abc");
+        System.out.println("hello t4");
     }
 }
